8dbb938d9e2fdcf706862d8c046992259047511a
Mark Egesdal <megesdal@teampraxis.com>
2018-01-21 10:26:07 -1000

    :lipstick: Reformat test files and remove unnecessary files

--- test/js-array.js
+++ test/js-array.js
@@ -1,67 +1,68 @@
 const { Query } = require('../query');
 const { executeQuery } = require('../js-array');
 
-	var data = [
-		{
-			'with/slash': 'slashed',
-			nested: {
-				property: 'value'
-			},
-			price: 10,
-			name: 'ten',
-			tags: [ 'fun', 'even' ]
-		},
-		{
-			price: 5,
-			name: 'five',
-			tags: [ 'fun' ]
-		},
-		{
-			price: 15,
-			name: 'five',
-			tags: [ 'foo' ]
-		}
-	];
+const data = [
+  {
+    'with/slash': 'slashed',
+    nested: {
+      property: 'value'
+    },
+    price: 10,
+    name: 'ten',
+    tags: ['fun', 'even']
+  },
+  {
+    price: 5,
+    name: 'five',
+    tags: ['fun']
+  },
+  {
+    price: 15,
+    name: 'five',
+    tags: ['foo']
+  }
+];
 
-		test('filtering', () => {
-			expect(executeQuery('price=lt=10', {}, data).length).toBe(1);
-			expect(executeQuery('price=lt=11', {}, data).length).toBe(2);
-			expect(executeQuery('nested/property=value', {}, data).length).toBe(1);
-			expect(executeQuery('with%2Fslash=slashed', {}, data).length).toBe(1);
-			expect(executeQuery('out(price,(5,10,15))', {}, data).length).toBe(0);
-			expect(executeQuery('out(price,(5))', {}, data).length).toBe(2);
-			expect(executeQuery('contains(tags,even)', {}, data).length).toBe(1);
-			expect(executeQuery('contains(tags,fun)', {}, data).length).toBe(2);
-			expect(executeQuery('excludes(tags,fun)', {}, data).length).toBe(1);
-			expect(executeQuery('excludes(tags,ne(fun))', {}, data).length).toBe(1);
-			expect(executeQuery('excludes(tags,ne(even))', {}, data).length).toBe(0);
-			// eq() on re: should trigger .match()
-			expect(executeQuery('price=match=10', {}, data)).toEqual([ data[0] ]);
-			// ne() on re: should trigger .not(.match())
-			expect(executeQuery('name=match=t.*', {}, data)).toEqual([ data[0] ]);
-			expect(executeQuery('name=match=glob:t*', {}, data)).toEqual([ data[0] ]);
-			expect(executeQuery(new Query().match('name', /t.*/), {}, data)).toEqual([data[0]]);
-		});
+test('filtering', () => {
+  expect(executeQuery('price=lt=10', {}, data).length).toBe(1);
+  expect(executeQuery('price=lt=11', {}, data).length).toBe(2);
+  expect(executeQuery('nested/property=value', {}, data).length).toBe(1);
+  expect(executeQuery('with%2Fslash=slashed', {}, data).length).toBe(1);
+  expect(executeQuery('out(price,(5,10,15))', {}, data).length).toBe(0);
+  expect(executeQuery('out(price,(5))', {}, data).length).toBe(2);
+  expect(executeQuery('contains(tags,even)', {}, data).length).toBe(1);
+  expect(executeQuery('contains(tags,fun)', {}, data).length).toBe(2);
+  expect(executeQuery('excludes(tags,fun)', {}, data).length).toBe(1);
+  expect(executeQuery('excludes(tags,ne(fun))', {}, data).length).toBe(1);
+  expect(executeQuery('excludes(tags,ne(even))', {}, data).length).toBe(0);
+  // eq() on re: should trigger .match()
+  expect(executeQuery('price=match=10', {}, data)).toEqual([data[0]]);
+  // ne() on re: should trigger .not(.match())
+  expect(executeQuery('name=match=t.*', {}, data)).toEqual([data[0]]);
+  expect(executeQuery('name=match=glob:t*', {}, data)).toEqual([data[0]]);
+  expect(executeQuery(new Query().match('name', /t.*/), {}, data)).toEqual([
+    data[0]
+  ]);
+});
 
-		test('filtering1', () => {
-			var data = [
-				{ 'path.1': [ 1, 2, 3 ] },
-				{ 'path.1': [ 9, 3, 7 ] }
-			];
+test('filtering1', () => {
+  const data = [{ 'path.1': [1, 2, 3] }, { 'path.1': [9, 3, 7] }];
 
-			expect(executeQuery('contains(path,3)&sort()', {}, data)).toEqual([]); // path is undefined
-			expect(executeQuery('contains(path.1,3)&sort()', {}, data)).toEqual(data); // 3 found in both
-			expect(executeQuery('excludes(path.1,3)&sort()', {}, data)).toEqual([]); // 3 found in both
-			expect(executeQuery('excludes(path.1,7)&sort()', {}, data)).toEqual([ data[0] ]); // 7 found in second
-		});
+  expect(executeQuery('contains(path,3)&sort()', {}, data)).toEqual([]); // path is undefined
+  expect(executeQuery('contains(path.1,3)&sort()', {}, data)).toEqual(data); // 3 found in both
+  expect(executeQuery('excludes(path.1,3)&sort()', {}, data)).toEqual([]); // 3 found in both
+  expect(executeQuery('excludes(path.1,7)&sort()', {}, data)).toEqual([
+    data[0]
+  ]); // 7 found in second
+});
 
-		test('sum', () => {
-			expect(executeQuery('sum(price)', {}, data)).toBe(30);
-		});
+test('sum', () => {
+  expect(executeQuery('sum(price)', {}, data)).toBe(30);
+});
 
-		test('aggregate', () => {
-			expect(executeQuery('aggregate(name,sum(price))', {}, data)).toEqual([
-				{0: 10, name: 'ten'},
-				{0: 20, name: 'five'}
-			]);
-		});
+test('aggregate', () => {
+  expect(executeQuery('aggregate(name,sum(price))', {}, data)).toEqual([
+    { 0: 10, name: 'ten' },
+    { 0: 20, name: 'five' }
+  ]);
+});
--- test/query.js
+++ test/query.js
@@ -1,220 +1,284 @@
 const { Query } = require('../query');
 const { parseQuery } = require('../parser');
 
-function hasKeys(it) {
-	var key;
+const hasKeys = it => {
+  if (it == null || typeof it !== 'object') {
+    return false;
+  }
 
-	if (it == null || typeof it !== 'object') {
-		return false;
-	}
-
-	for (key in it) {
-		if (it.hasOwnProperty(key)) {
-			return true;
-		}
-	}
-	return false;
-}
+  return Object.keys(it).length > 0;
+};
 
 const supportsDateString = !isNaN(new Date('2009')),
-		queryPairs = {
-			arrays: {
-				a: { name: 'and', args: [ 'a' ]},
-				'(a)': { name: 'and', args: [[ 'a' ]]},
-				'a,b,c': { name: 'and', args: [ 'a', 'b', 'c' ]},
-				'(a,b,c)': { name: 'and', args: [[ 'a', 'b', 'c']]},
-				'a(b)': { name: 'and', args: [{ name: 'a', args: [ 'b' ]}]},
-				'a(b,c)': { name: 'and', args: [{ name: 'a', args: [ 'b', 'c' ]}]},
-				'a((b),c)': { name: 'and', args: [{ name: 'a', args: [ [ 'b' ], 'c' ]}]},
-				'a((b,c),d)': { name: 'and', args: [{ name: 'a', args: [ [ 'b', 'c' ], 'd' ]}]},
-				'a(b/c,d)': { name: 'and', args: [{ name: 'a', args: [ [ 'b', 'c' ], 'd' ]}]},
-				'a(b)&c(d(e))': { name: 'and', args:[
-					{ name: 'a', args: [ 'b' ]},
-					{ name: 'c', args: [ { name: 'd', args: [ 'e' ]} ]}
-				]}
-			},
-			'dot-comparison': {
-				'foo.bar=3': { name: 'and', args: [{ name: 'eq', args: [ 'foo.bar', 3 ]}]},
-				'select(sub.name)': {
-					name: 'and',
-					args: [ { name: 'select', args: [ 'sub.name' ]} ],
-					cache: { select: [ 'sub.name' ]}
-				}
-			},
-			equality: {
-				'eq(a,b)': { name: 'and', args:[{ name: 'eq', args: [ 'a', 'b' ]}]},
-				'a=eq=b': 'eq(a,b)',
-				'a=b': 'eq(a,b)'
-			},
-			inequality: {
-				'ne(a,b)': { name: 'and', args: [{ name: 'ne', args: [ 'a', 'b' ]}]},
-				'a=ne=b': 'ne(a,b)',
-				'a!=b': 'ne(a,b)'
-			},
-			'less-than': {
-				'lt(a,b)': { name: 'and', args: [{ name: 'lt', args: [ 'a', 'b' ]}]},
-				'a=lt=b': 'lt(a,b)',
-				'a<b': 'lt(a,b)'
-			},
-			'less-than-equals': {
-				'le(a,b)': { name: 'and', args: [{ name: 'le', args: [ 'a','b' ]}]},
-				'a=le=b': 'le(a,b)',
-				'a<=b': 'le(a,b)'
-			},
-			'greater-than': {
-				'gt(a,b)': { name: 'and', args: [{ name: 'gt', args: [ 'a', 'b' ]}]},
-				'a=gt=b': 'gt(a,b)',
-				'a>b': 'gt(a,b)'
-			},
-			'greater-than-equals': {
-				'ge(a,b)': { name: 'and', args: [{ name: 'ge', args: [ 'a', 'b' ]}]},
-				'a=ge=b': 'ge(a,b)',
-				'a>=b': 'ge(a,b)'
-			},
-			'nested comparisons': {
-				'a(b(le(c,d)))': { name: 'and', args: [
-					{ name: 'a', args: [{ name: 'b', args: [{ name: 'le', args: [ 'c', 'd' ]}]}]}
-				]},
-				'a(b(c=le=d))': 'a(b(le(c,d)))',
-				'a(b(c<=d))': 'a(b(le(c,d)))'
-			},
-			'arbitrary FIQL desugaring': {
-				'a=b=c': { name: 'and', args: [{ name: 'b', args: [ 'a', 'c' ]}]},
-				'a(b=cd=e)': { name: 'and', args: [{ name: 'a', args: [{ name: 'cd', args: [ 'b', 'e' ]}]}]}
-			},
-			'and grouping': {
-				'a&b&c': { name: 'and', args: [ 'a', 'b', 'c' ]},
-				'a(b)&c': { name: 'and', args: [ { name: 'a', args: [ 'b' ] }, 'c' ]},
-				'a&(b&c)': { name: 'and', args: [ 'a', { name: 'and', args: [ 'b', 'c' ]}]}
-			},
-			'or grouping': {
-				'(a|b|c)': { name: 'and', args: [{ name: 'or', args: [ 'a', 'b', 'c' ]}]},
-				'(a(b)|c)': { name: 'and', args: [{ name: 'or', args: [ { name: 'a', args: [ 'b' ]}, 'c' ]}]}
-			},
-			'complex grouping': {
-				'a&(b|c)': { name: 'and', args: [ 'a', { name: 'or', args: [ 'b', 'c' ]}]},
-				'a|(b&c)': { name: 'or', args: [ 'a', { name: 'and', args: [ 'b', 'c' ]}]},
-				'a(b(c<d,e(f=g)))': { 'name': 'and', 'args': [{ 'name': 'a', 'args': [{ 'name': 'b', 'args': [{ 'name': 'lt', 'args': ['c', 'd']}, {'name':'e', 'args': [{ 'name': 'eq', 'args': [ 'f', 'g' ]}]}]}]}]}
-			},
-			'complex comparisons': {
+  queryPairs = {
+    arrays: {
+      a: { name: 'and', args: ['a'] },
+      '(a)': { name: 'and', args: [['a']] },
+      'a,b,c': { name: 'and', args: ['a', 'b', 'c'] },
+      '(a,b,c)': { name: 'and', args: [['a', 'b', 'c']] },
+      'a(b)': { name: 'and', args: [{ name: 'a', args: ['b'] }] },
+      'a(b,c)': { name: 'and', args: [{ name: 'a', args: ['b', 'c'] }] },
+      'a((b),c)': { name: 'and', args: [{ name: 'a', args: [['b'], 'c'] }] },
+      'a((b,c),d)': {
+        name: 'and',
+        args: [{ name: 'a', args: [['b', 'c'], 'd'] }]
+      },
+      'a(b/c,d)': {
+        name: 'and',
+        args: [{ name: 'a', args: [['b', 'c'], 'd'] }]
+      },
+      'a(b)&c(d(e))': {
+        name: 'and',
+        args: [
+          { name: 'a', args: ['b'] },
+          { name: 'c', args: [{ name: 'd', args: ['e'] }] }
+        ]
+      }
+    },
+    'dot-comparison': {
+      'foo.bar=3': {
+        name: 'and',
+        args: [{ name: 'eq', args: ['foo.bar', 3] }]
+      },
+      'select(sub.name)': {
+        name: 'and',
+        args: [{ name: 'select', args: ['sub.name'] }],
+        cache: { select: ['sub.name'] }
+      }
+    },
+    equality: {
+      'eq(a,b)': { name: 'and', args: [{ name: 'eq', args: ['a', 'b'] }] },
+      'a=eq=b': 'eq(a,b)',
+      'a=b': 'eq(a,b)'
+    },
+    inequality: {
+      'ne(a,b)': { name: 'and', args: [{ name: 'ne', args: ['a', 'b'] }] },
+      'a=ne=b': 'ne(a,b)',
+      'a!=b': 'ne(a,b)'
+    },
+    'less-than': {
+      'lt(a,b)': { name: 'and', args: [{ name: 'lt', args: ['a', 'b'] }] },
+      'a=lt=b': 'lt(a,b)',
+      'a<b': 'lt(a,b)'
+    },
+    'less-than-equals': {
+      'le(a,b)': { name: 'and', args: [{ name: 'le', args: ['a', 'b'] }] },
+      'a=le=b': 'le(a,b)',
+      'a<=b': 'le(a,b)'
+    },
+    'greater-than': {
+      'gt(a,b)': { name: 'and', args: [{ name: 'gt', args: ['a', 'b'] }] },
+      'a=gt=b': 'gt(a,b)',
+      'a>b': 'gt(a,b)'
+    },
+    'greater-than-equals': {
+      'ge(a,b)': { name: 'and', args: [{ name: 'ge', args: ['a', 'b'] }] },
+      'a=ge=b': 'ge(a,b)',
+      'a>=b': 'ge(a,b)'
+    },
+    'nested comparisons': {
+      'a(b(le(c,d)))': {
+        name: 'and',
+        args: [
+          {
+            name: 'a',
+            args: [{ name: 'b', args: [{ name: 'le', args: ['c', 'd'] }] }]
+          }
+        ]
+      },
+      'a(b(c=le=d))': 'a(b(le(c,d)))',
+      'a(b(c<=d))': 'a(b(le(c,d)))'
+    },
+    'arbitrary FIQL desugaring': {
+      'a=b=c': { name: 'and', args: [{ name: 'b', args: ['a', 'c'] }] },
+      'a(b=cd=e)': {
+        name: 'and',
+        args: [{ name: 'a', args: [{ name: 'cd', args: ['b', 'e'] }] }]
+      }
+    },
+    'and grouping': {
+      'a&b&c': { name: 'and', args: ['a', 'b', 'c'] },
+      'a(b)&c': { name: 'and', args: [{ name: 'a', args: ['b'] }, 'c'] },
+      'a&(b&c)': { name: 'and', args: ['a', { name: 'and', args: ['b', 'c'] }] }
+    },
+    'or grouping': {
+      '(a|b|c)': { name: 'and', args: [{ name: 'or', args: ['a', 'b', 'c'] }] },
+      '(a(b)|c)': {
+        name: 'and',
+        args: [{ name: 'or', args: [{ name: 'a', args: ['b'] }, 'c'] }]
+      }
+    },
+    'complex grouping': {
+      'a&(b|c)': { name: 'and', args: ['a', { name: 'or', args: ['b', 'c'] }] },
+      'a|(b&c)': { name: 'or', args: ['a', { name: 'and', args: ['b', 'c'] }] },
+      'a(b(c<d,e(f=g)))': {
+        name: 'and',
+        args: [
+          {
+            name: 'a',
+            args: [
+              {
+                name: 'b',
+                args: [
+                  { name: 'lt', args: ['c', 'd'] },
+                  { name: 'e', args: [{ name: 'eq', args: ['f', 'g'] }] }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    },
+    'complex comparisons': {},
+    'string coercion': {
+      'a(string)': { name: 'and', args: [{ name: 'a', args: ['string'] }] },
+      'a(string:b)': { name: 'and', args: [{ name: 'a', args: ['b'] }] },
+      'a(string:1)': { name: 'and', args: [{ name: 'a', args: ['1'] }] }
+    },
+    'number coercion': {
+      'a(number)': { name: 'and', args: [{ name: 'a', args: ['number'] }] },
+      'a(number:1)': { name: 'and', args: [{ name: 'a', args: [1] }] }
+      //'a(number:b)': { name: 'and', args: [{ name: 'a', args: [ NaN ]}]} // supposed to throw an error
+    },
+    'date coercion': {
+      //FIXME do we need proper ISO date subset parsing?
+      'a(date)': { name: 'and', args: [{ name: 'a', args: ['date'] }] },
+      'a(date:2009)': supportsDateString && {
+        name: 'and',
+        args: [{ name: 'a', args: [new Date('2009')] }]
+      },
+      'a(date:1989-11-21)': supportsDateString && {
+        name: 'and',
+        args: [{ name: 'a', args: [new Date('1989-11-21')] }]
+      },
+      'a(date:1989-11-21T00:21:00.21Z)': {
+        name: 'and',
+        args: [
+          { name: 'a', args: [new Date(Date.UTC(1989, 10, 21, 0, 21, 0, 21))] }
+        ]
+      },
+      'a(date:1989-11-21T00:21:00Z)': {
+        name: 'and',
+        args: [
+          { name: 'a', args: [new Date(Date.UTC(1989, 10, 21, 0, 21, 0))] }
+        ]
+      }
+      //'a(date:b)': { name: 'and', args: [{ name: 'a', args: [ new Date(NaN) ]}]} // XXX?// supposed to throw an error
+    },
+    'boolean coercion': {
+      'a(true)': { name: 'and', args: [{ name: 'a', args: [true] }] },
+      'a(false)': { name: 'and', args: [{ name: 'a', args: [false] }] },
+      'a(boolean:true)': { name: 'and', args: [{ name: 'a', args: [true] }] }
+    },
+    'null coercion': {
+      'a(null)': { name: 'and', args: [{ name: 'a', args: [null] }] },
+      'a(auto:null)': { name: 'and', args: [{ name: 'a', args: [null] }] },
+      'a(string:null)': { name: 'and', args: [{ name: 'a', args: ['null'] }] }
+    },
+    'complex coercion': {
+      '(a=b|c=d)&(e=f|g=1)': {
+        name: 'and',
+        args: [
+          {
+            name: 'or',
+            args: [
+              { name: 'eq', args: ['a', 'b'] },
+              { name: 'eq', args: ['c', 'd'] }
+            ]
+          },
+          {
+            name: 'or',
+            args: [
+              { name: 'eq', args: ['e', 'f'] },
+              { name: 'eq', args: ['g', 1] }
+            ]
+          }
+        ]
+      }
+    }
+  };
 
-			},
-			'string coercion': {
-				'a(string)': { name: 'and', args: [{ name: 'a', args: [ 'string' ]}]},
-				'a(string:b)': { name: 'and', args: [{ name: 'a', args: [ 'b' ]}]},
-				'a(string:1)': { name: 'and', args: [{ name: 'a', args: [ '1' ]}]}
-			},
-			'number coercion': {
-				'a(number)': { name: 'and', args: [{ name: 'a', args: [ 'number' ]}]},
-				'a(number:1)': {name: 'and', args: [{ name: 'a', args: [ 1 ]}]}
-				//'a(number:b)': { name: 'and', args: [{ name: 'a', args: [ NaN ]}]} // supposed to throw an error
-			},
-			'date coercion': {
-				//FIXME do we need proper ISO date subset parsing?
-				'a(date)': { name: 'and', args: [{ name: 'a', args: [ 'date' ]}]},
-				'a(date:2009)': supportsDateString && { name: 'and', args: [{ name: 'a', args: [ new Date('2009') ]}]},
-				'a(date:1989-11-21)': supportsDateString && {name: 'and', args:[{name: 'a', args: [(new Date('1989-11-21'))]}]},
-				'a(date:1989-11-21T00:21:00.21Z)': {name: 'and', args:[{name: 'a', args: [(new Date(Date.UTC(1989, 10, 21, 0, 21, 0, 21)))]}]},
-				'a(date:1989-11-21T00:21:00Z)': {name: 'and', args:[{name: 'a', args: [(new Date(Date.UTC(1989, 10, 21, 0, 21, 0)))]}]}
-				//'a(date:b)': { name: 'and', args: [{ name: 'a', args: [ new Date(NaN) ]}]} // XXX?// supposed to throw an error
-			},
-			'boolean coercion': {
-				'a(true)': { name: 'and', args: [{ name: 'a', args: [ true ]}]},
-				'a(false)': { name: 'and', args: [{ name: 'a', args: [ false ]}]},
-				'a(boolean:true)': { name: 'and', args: [{ name: 'a', args: [ true ]}]}
-			},
-			'null coercion': {
-				'a(null)': { name: 'and', args: [{ name: 'a', args: [ null ]}]},
-				'a(auto:null)': { name: 'and', args: [{ name: 'a', args: [ null ]}]},
-				'a(string:null)': { name: 'and', args: [{ name: 'a', args: [ 'null' ]}]}
-			},
-			'complex coercion': {
-				'(a=b|c=d)&(e=f|g=1)': { name: 'and', args: [
-					{ name: 'or', args: [{ name: 'eq', args: [ 'a', 'b' ]}, { name: 'eq', args: [ 'c', 'd' ]}]},
-					{ name: 'or', args: [{ name: 'eq', args: [ 'e', 'f' ]}, { name: 'eq', args: [ 'g', 1 ]}]}
-				]}
-			}
-		};
+test('parsing', () => {
+  for (let group in queryPairs) {
+    const pairs = queryPairs[group];
+    for (let key in pairs) {
+      let expected = pairs[key];
 
-		test('parsing', () => {
-			for (let group in queryPairs) {
-				const pairs = queryPairs [ group ];
-				for (let key in pairs) {
-					let expected = pairs[key];
+      // skip tests which don't have an expected value
+      if (!expected) {
+        continue;
+      }
 
-					// skip tests which don't have an expected value
-					if (!expected) {
-						continue;
-					}
+      const actual = parseQuery(key);
 
-					const actual = parseQuery(key);
+      if (!hasKeys(actual.cache)) {
+        delete actual.cache;
+      }
 
-					if (!hasKeys(actual.cache)) {
-						delete actual.cache;
-					}
+      if (typeof expected === 'string') {
+        expected = parseQuery(expected);
+      }
 
-					if (typeof expected === 'string') {
-						expected = parseQuery(expected);
-					}
+      if (!hasKeys(expected.cache)) {
+        delete expected.cache;
+      }
 
-					if (!hasKeys(expected.cache)) {
-						delete expected.cache;
-					}
+      expect(actual).toEqual(expected);
+    }
+  }
+});
 
-					// someone decided that matching constructors is necessary for deep equality
-					// see https://github.com/theintern/intern/issues/284
-					// the deepEqual assertion also fails due to properties like toString so this assertion seems to
-					// be the most suitable.
-					expect(actual).toEqual(expected); //assert.strictEqual(JSON.stringify(actual), JSON.stringify(expected));
-				}
-			}
-		});
+test('behavior', () => {
+  //assert.error(parseQuery(), "parseQuery requires a string");
+  expect(parseQuery('')).toBeInstanceOf(Query);
+  expect(parseQuery('a=b')).toBeInstanceOf(Query);
+  //assert.error(parseQuery('?a=b'), 'cannot begin with a ?');
+});
 
-		test('behavior', () => {
-			//assert.error(parseQuery(), "parseQuery requires a string");
-			expect(parseQuery('')).toBeInstanceOf(Query);
-			expect(parseQuery('a=b')).toBeInstanceOf(Query);
-			//assert.error(parseQuery('?a=b'), 'cannot begin with a ?');
-		});
+test('bind parameters', () => {
+  // TODO
+  let parsed;
+  parsed = parseQuery('in(id,$1)', [['a', 'b', 'c']]);
+  expect(parsed).toEqual({
+    name: 'and',
+    args: [{ name: 'in', args: ['id', ['a', 'b', 'c']] }],
+    cache: {}
+  });
+  parsed = parseQuery('eq(id,$1)', ['a']);
+  expect(parsed).toEqual({
+    name: 'and',
+    args: [{ name: 'eq', args: ['id', 'a'] }],
+    cache: { id: 'a' }
+  });
+});
 
-		test('bind parameters', () => {
-			// TODO
-			var parsed;
-			parsed = parseQuery('in(id,$1)', [['a','b','c']]);
-			expect(parsed).toEqual({
-				name: 'and',
-				args: [{ name: 'in', args: [ 'id', [ 'a', 'b', 'c' ]]}],
-				cache: {}
-			});
-			parsed = parseQuery('eq(id,$1)', [ 'a' ]);
-			expect(parsed).toEqual({
-				name: 'and',
-				args: [{ name: 'eq', args: ['id', 'a']}],
-				cache: {id: 'a'}
-			});
-		});
+test('stringification', () => {
+  // TODO
+  let parsed;
+  parsed = parseQuery('eq(id1,RE:%5Eabc%5C%2F)');
+  // Hmmm. deepEqual gives null for regexps?
+  expect(parsed.args[0].args[1].toString()).toBe(/^abc\//.toString());
+  //assert.deepEqual(parsed, {name: 'and', args: [{name: 'eq', args: ['id1', /^abc\//]}]});
+  expect(new Query().eq('_1', /GGG(EE|FF)/i) + '').toBe(
+    'eq(_1,re:GGG%28EE%7CFF%29)'
+  );
+  parsed = parseQuery('eq(_1,re:GGG%28EE%7CFF%29)');
+  expect(parsed.args[0].args[1].toString()).toBe(/GGG(EE|FF)/i.toString());
+  //assert.ok(Query().eq('_1',/GGG(EE|FF)/)+'' === 'eq(_1,RE:GGG%28EE%7CFF%29)');
+  // string to array and back
+  const str = 'somefunc(and(1),(a,b),(10,(10,1)),(a,b.c))';
+  expect(parseQuery(str) + '').toBe(str);
+  // quirky arguments
+  const name = ['a/b', 'c.d'];
+  expect(parseQuery(new Query().eq(name, 1) + '') + '').toBe(
+    'eq((a%2Fb,c.d),1)'
+  );
+  expect(parseQuery(new Query().eq(name, 1) + '').args[0].args[0]).toEqual(
+    name
+  );
+});
 
-		test('stringification', () => {
-			// TODO
-			var parsed;
-			parsed = parseQuery('eq(id1,RE:%5Eabc%5C%2F)');
-			// Hmmm. deepEqual gives null for regexps?
-			expect(parsed.args[0].args[1].toString()).toBe(/^abc\//.toString());
-			//assert.deepEqual(parsed, {name: 'and', args: [{name: 'eq', args: ['id1', /^abc\//]}]});
-			expect(new Query().eq('_1',/GGG(EE|FF)/i) + '').toBe('eq(_1,re:GGG%28EE%7CFF%29)');
-			parsed = parseQuery('eq(_1,re:GGG%28EE%7CFF%29)');
-			expect(parsed.args[0].args[1].toString()).toBe(/GGG(EE|FF)/i.toString());
-			//assert.ok(Query().eq('_1',/GGG(EE|FF)/)+'' === 'eq(_1,RE:GGG%28EE%7CFF%29)');
-			// string to array and back
-			var str = 'somefunc(and(1),(a,b),(10,(10,1)),(a,b.c))';
-			expect(parseQuery(str) + '').toBe(str);
-			// quirky arguments
-			var name = ['a/b','c.d'];
-			expect(parseQuery(new Query().eq(name,1) + '') + '').toBe('eq((a%2Fb,c.d),1)');
-			expect(parseQuery(new Query().eq(name,1) + '').args[0].args[0]).toEqual(name);
-		});
-
-		test('matches', () => {
-			var query = new Query().match('name', /Will*/);
-			expect('' + query).toBe('match(name,RE:Will*)');
-		});
+test('matches', () => {
+  const query = new Query().match('name', /Will*/);
+  expect('' + query).toBe('match(name,RE:Will*)');
+});
